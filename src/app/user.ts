bfe81786ba08a876630855a650e704fc2606df2a
peter max <petermax2004@gmail.com>
2019-06-08 16:42:45 +0300

    Add the constructore to the user.ts class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,5 @@
 export class User {
+    constructor( public username:string, public userData: any, public repo:any, public search:boolean ){
+        this.username = username, this.userData = userData , this.repo = repo, this.search = false
+    }
 }
