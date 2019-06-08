650e29f62783921a83fd3d576d93dfa20f26a0ad
peter max <petermax2004@gmail.com>
2019-06-08 12:07:20 +0300

    Add the form building blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RepoComponent } from './repo/repo.component';
+import { GitFormComponent } from './git-form/git-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    RepoComponent
+    RepoComponent,
+    GitFormComponent
   ],
   imports: [
     BrowserModule
