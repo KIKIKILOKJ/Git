961d30487d35a5031764b94f01fb77b561c23c1a
peter max <petermax2004@gmail.com>
2019-06-08 12:18:49 +0300

    Add the the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -14,7 +15,8 @@ import { GitFormComponent } from './git-form/git-form.component';
     GitFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
