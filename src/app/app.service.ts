da4df43c4103da85218ed359e78c8d8288837984
peter max <petermax2004@gmail.com>
2019-06-08 17:26:18 +0300

    Add the github information for retrival purposes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http'
+import { access } from 'fs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getProfile(name:string){
+    return this.http.get(environment.apiUrl+name+"?access_token="+environment.apikey);
+  }
+
+  getRepos(_url: any){
+    return this.http.get( _url );
+  }
+
+  getRepo(name){
+    return this.http.get("https://api.github.com/repos/"+name);
+  }
 }
