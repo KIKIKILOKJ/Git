f1abd8ffb92e99446244d01bed9b22c7d80aa4f8
peter max <petermax2004@gmail.com>
2019-06-08 19:20:05 +0300

    Add the routing functionality

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule, } from '@angular/router';
+import { UserComponent } from '../user/user.component';
+import { Repo } from '../repo';
+import { RepoComponent } from '../repo/repo.component';
+
+const routes: Routes=[
+  {
+    path:'',
+    component: UserComponent
+  },
+
+  {
+    path:'repos/:name',
+    component:RepoComponent
+  },
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+
+  exports:[
+    RouterModule
   ],
   declarations: []
 })
