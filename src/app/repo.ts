e74c2a38e05046612872b4d2e52da18ac0811d60
peter max <petermax2004@gmail.com>
2019-06-10 13:57:08 +0300

    Auto-generated commit

--- src/app/repo.ts
+++
@@ -1,5 +0,0 @@
-export class Repo {
-    constructor( public repoName:string, public repoInfo:string ){
-        this.repoName = repoName, this.repoInfo = repoInfo
-    }
-}
--- src/app/user.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-    constructor( public username:string, public userData: any, public repo:any, public search:boolean ){
-        this.username = username, this.userData = userData , this.repo = repo, this.search = false
-    }
-}
